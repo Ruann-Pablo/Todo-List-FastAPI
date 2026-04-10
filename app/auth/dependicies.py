3e453ba3dc79daa80ccb259cec79d5d61f3e3380
Ruann-Pablo <ruannprime@gmail.com>
2026-04-10 01:15:06 -0300

    refatora função get_current_user: substitui decode_access_token por decode_token e adiciona verificação de tipo de token

--- app/auth/dependicies.py
+++ app/auth/dependicies.py
@@ -2,7 +2,7 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from app.database.connection import get_db
-from app.auth.security import decode_access_token
+from app.auth.security import decode_token
 from app.models.user_model import User
 from sqlalchemy import select
 
@@ -12,7 +12,12 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/user/login")
 def get_current_user(
     token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
 ):
-    payload = decode_access_token(token)
+    payload = decode_token(token)
+
+    if payload.get("type") != "access":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Acesso negado"
+        )
 
     user_id = payload.get("sub")
 
